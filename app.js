f39fe4d31100e41532947419d822ba670d64a992
Victor <victor.du.77@hotmail.fr>
2020-03-26 14:53:11 +0100

    Add middleware CORS + add GET route

--- app.js
+++ app.js
@@ -3,22 +3,32 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('Requête reçue');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({message: "Nous avons bien reçu la requete"});
-    next();
-});
-
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès');
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://picsum.photos/200/300',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://picsum.photos/200/300',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
 });
 
 module.exports = app;
