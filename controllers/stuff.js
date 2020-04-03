0324a088d3f231405605a061af758b8d24bb5f5e
Victor <victor.du.77@hotmail.fr>
2020-04-03 12:36:10 +0200

    Add fs to delete img

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,4 +1,5 @@
 const Thing = require('../models/Thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
     const thingObject = JSON.parse(req.body.thing);
@@ -45,15 +46,30 @@ exports.modifyThing = (req, res, next) => {
 };
 
 exports.deleteThing = (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id}).then(
-        () => {
-            res.status(200).json({
-                message: 'Deleted!'
-            });
+    Thing.findOne({_id: req.params.id})
+    .then(
+        thing => {
+            const fileName = thing.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${ fileName }`, () => {
+                Thing.deleteOne({_id: req.params.id})
+                    .then(
+                        () => {
+                            res.status(200).json({
+                                message: 'Deleted!'
+                            });
+                        }
+                    ).catch(
+                    (error) => {
+                        res.status(400).json({
+                            error: error
+                        });
+                    }
+                );
+            })
         }
     ).catch(
         (error) => {
-            res.status(400).json({
+            res.status(500).json({
                 error: error
             });
         }
